5838840198abb602553f6e3b17c1b2e5771498ce
jsanch387 <75911707+jsanch387@users.noreply.github.com>
2025-09-24 20:54:35 -0500

    favicon updats

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,15 +70,6 @@ export const metadata: Metadata = {
     images: ['/og-image.jpg'], // You can add this later
     creator: '@servicelink', // Update with your actual Twitter handle
   },
-  icons: {
-    icon: [
-      { url: '/favicon.ico', sizes: 'any' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-    ],
-    shortcut: '/favicon.ico',
-    apple: '/favicon.png',
-  },
 };
 
 export default function RootLayout({
@@ -89,21 +80,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/favicon.png" />
         <meta name="theme-color" content="#f97316" />
       </head>
       <body
